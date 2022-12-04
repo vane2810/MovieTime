b0bc7d960bd185c63af0af6a9ef76b793f3b84f9
vane2810 <olgavanessasf@gmail.com>
2022-12-04 10:53:28 -0600

    Agregar archivos de proyecto.

---
+++ MovieTime/Data/MovieTimeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTime.Data
+{
+    public class MovieTimeContext : DbContext
+    {
+        public MovieTimeContext(DbContextOptions<MovieTimeContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- MovieTime/Startup.cs
+++ MovieTime/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieTime.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,9 @@ namespace MovieTime
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MovieTimeContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MovieTimeContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
