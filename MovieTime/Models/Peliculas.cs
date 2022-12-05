cfb914dfaa58e50d59eaa55bdc7f71e19b95759b
Hazel Granados <hazelbgranados@gmail.com>
2022-12-04 16:13:30 -0900

    1

---
+++ MovieTime/Models/Peliculas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTime.Models
+{
+    public class Peliculas
+    {
+        public int Id { get; set; }
+
+        public string? Titulo { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        public string? Genero { get; set; }
+
+        public decimal Precio  { get; set; }
+    }
+}
