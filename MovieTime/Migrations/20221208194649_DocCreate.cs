e05fcd48ca72216c70f10135678c2d8a177ce924
vane2810 <olgavanessasf@gmail.com>
2022-12-08 13:51:30 -0600

    1

---
+++ MovieTime/Migrations/20221208194649_DocCreate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieTime.Migrations
+{
+    public partial class DocCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Documentales");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Precio",
+                table: "Documentales",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
