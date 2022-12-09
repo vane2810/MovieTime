24268b195ee80ed1294c68ae220f4745b2c4131c
Hazel Granados <hazelbgranados@gmail.com>
2022-12-08 17:35:20 -0900

    1

---
+++ MovieTime/Migrations/20221209023446_InicialCreate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieTime.Migrations
+{
+    public partial class InicialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Series");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Precio",
+                table: "Series",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
