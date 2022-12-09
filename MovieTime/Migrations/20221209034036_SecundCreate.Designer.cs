e31d6aab9fb0c224f7c6932cecb2122d3bbb8824
Hazel Granados <hazelbgranados@gmail.com>
2022-12-08 18:50:27 -0900

    1

--- MovieTime/Migrations/20221209034036_SecundCreate.Designer.cs
+++ MovieTime/Migrations/20221209034036_SecundCreate.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieTime.Data;
 
 namespace MovieTime.Migrations
 {
     [DbContext(typeof(MovieTimeContext))]
-    partial class MovieTimeContextModelSnapshot : ModelSnapshot
+    [Migration("20221209034036_SecundCreate")]
+    partial class SecundCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MovieTime/Migrations/20221209034036_SecundCreate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieTime.Migrations
+{
+    public partial class SecundCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Peliculas");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Precio",
+                table: "Peliculas",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
